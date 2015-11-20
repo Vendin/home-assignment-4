5bf8df4eae2c7b80dc7b79ce95e0605fd28593eb
mikrut <mihanik01@mail.ru>
2015-11-20 14:28:49 +0300

    Modified README, added simple assertions, deleted grid as unnecessary for test launches

--- test/test.py
+++ test/test.py
@@ -9,6 +9,7 @@ from selenium.webdriver import DesiredCapabilities, Remote
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 from urlparse import urlparse, urljoin
 
@@ -26,6 +27,13 @@ class Page:
         self.driver.get(url)
         self.driver.maximize_window()
 
+    def check_element_exists(self, xpath):
+        try:
+            self.driver.find_element_by_xpath(xpath)
+        except NoSuchElementException:
+            return False
+        return True
+
     def close(self):
         self.driver.close()
 
@@ -36,42 +44,74 @@ class Component(object):
 class MainPage(Page):
     PATH = ''
 
+    LOGIN_BTN_XPATH = '//span[contains(@class, "ph-button__inner_profilemenu_signin")]'
+    MENU_BTN_XPATH = '//span[@xname="clb2490734"]'
+
     @property
     def login_button(self):
-        return LoginButton(self.driver)
+        return Button(self.driver, self.LOGIN_BTN_XPATH)
+
+    def login_button_exists(self):
+        return Page.check_element_exists(self, self.LOGIN_BTN_XPATH)
+
+    @property
+    def menu_button(self):
+        return Button(self.driver, self.MENU_BTN_XPATH)
+
+    def menu_button_exists(self):
+        return Page.check_element_exists(self, self.MENU_BTN_XPATH)
 
     def open(self):
         Page.open(self)
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH,
-                LoginButton.XPATH))
-        )
+        try:
+            WebDriverWait(self.driver, 10).until(
+                expected_conditions.presence_of_element_located((By.XPATH, self.LOGIN_BTN_XPATH))
+            )
+        except TimeoutException:
+            print "No login button found"
 
-class LoginButton(Component):
-    XPATH = '//span[contains(@class, "ph-button__inner_profilemenu_signin")]'
+class Button(Component):
+    def __init__(self, driver, xpath):
+        Component.__init__(self, driver)
+        self.xpath = xpath
+        self.button = self.driver.find_element_by_xpath(self.xpath)
 
-    def open_form(self):
-        self.driver.find_element_by_xpath(self.XPATH).click()
-
+    def click(self):
+        self.button.click()
 
 class TargetTest(unittest.TestCase):
     USERNAME = u'name'
 
     def setUp(self):
-        browser = os.environ.get('TTHA2BROWSER', 'FIREFOX')
+        browser = os.environ.get('TTHA4BROWSER', 'FIREFOX')
+        self.password = os.environ.get('TTHA4PASSWORD')
+        self.login = os.environ.get('TTHA4LOGIN')
+        self.mode = os.environ.get('TTHA4MODE', 'mail')
+
+        if browser == 'CHROME':
+            self.driver = webdriver.Chrome()
+        else:
+            self.driver = webdriver.Firefox()
+        """
+        This is for grid
         self.driver = Remote(
-            command_executor='http://127.0.0.1:4444/wd/hub',
+            command_executor='http://localhost:4444/wd/hub',
             desired_capabilities=getattr(DesiredCapabilities, browser).copy()
         )
+        """
         self.main_page = MainPage(self.driver)
-        self.main_page.open()
 
-
-    def test(self):
-        login_button = self.main_page.login_button
-        login_button.open_form()
-        self.assertEqual(3, 1)
 
     def tearDown(self):
         self.main_page.close()
+        self.driver.quit()
+
+    def test_login_button_present(self):
+        self.main_page.open()
+        self.assertTrue(self.main_page.login_button_exists())
+
+    def test_menu_button_present(self):
+        self.main_page.open()
+        self.assertTrue(self.main_page.menu_button_exists())
+
 

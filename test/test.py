dadbe93331feb8cf7e66206d8ea35da5194f884a
Mihail Krutyakov <mihanik01@mail.ru>
2015-11-21 19:51:27 +0300

    Added audience creation/deletion + tests

--- test/test.py
+++ test/test.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 from helpers import Page, Component, check_visible, check_invisible
 from main_page import MainPage
@@ -30,6 +31,8 @@ class AudiencePage(Page):
     USRLIST_BTN_XPATH  = BTN_XPATH_TMPL % 6
     SRCHLIST_BTN_XPATH = BTN_XPATH_TMPL % 7
     PRICELST_BTN_XPATH = BTN_XPATH_TMPL % 8
+    
+    AUD_CREATE_BTN_XPATH = '//input[contains(@class, "audience-page__audience-create-submit")]'
     
     def get_top_ctr_adder(self):
         top_btn = self.driver.find_element_by_xpath(self.TOP_BTN_XPATH)
@@ -58,6 +61,22 @@ class AudiencePage(Page):
             )
         except TimeoutException:
             print "No header found"
+    
+    def has_create_aud_btn(self):
+        return check_visible(self.AUD_CREATE_BTN_XPATH, self.driver)
+    
+    def get_create_aud_form(self):
+        create_aud_btn = self.driver.find_element_by_xpath(self.AUD_CREATE_BTN_XPATH)
+        create_aud_btn.click()
+        aud_form = AudienceForm(self.driver)
+        check_visible(aud_form.FORM_XPATH, self.driver)
+        return aud_form
+    
+    def get_aud_element(self, name):
+        if check_visible(AudienceElement.HDR_XPATH_TMPL % name, self.driver):
+            return AudienceElement(self.driver, name);
+        else:
+            return None
     
 
 class TopCtrAdder(Component):
@@ -120,6 +139,69 @@ class VKGroupAdder(GroupAdder):
 class VKAppAdder(GroupAdder):
     def check_has_helper(self):
         return check_visible(self.HELPER_XPATH, self.driver, ttl=5)
+
+class AudienceForm(Component):
+    FORM_XPATH = '//*[contains(@class, "audience-form")][1]'
+    
+    def __init__(self, driver, xpath=None):
+        if xpath is None:
+            xpath = AudienceForm.FORM_XPATH 
+        Component.__init__(self, driver)
+        self._SELF_XPATH = xpath
+        
+        self._NAME_XPATH = self._SELF_XPATH+'/descendant::input[contains(@class, "audience-form__audience-name-input")]'
+        self._SUBMIT_XPATH = self._SELF_XPATH+'/descendant::input[@type="submit"]'
+        self._DELETE_XPATH = self._SELF_XPATH+'/descendant::span[contains(@class, "audience-form__delete")]'
+        self._DEL_YES_XPATH = self._SELF_XPATH+'/descendant::input[contains(@class, "audience-form__confirm-button") and @data-type="yes"]';
+        
+        self._MARK_ALL_XPATH = self._SELF_XPATH+'/descendant::input[@id="-box-all"]'
+        
+        self._SOURCE_ERR_XPATH = self._SELF_XPATH + '/descendant::div[contains(@class, "audience-form__no-source-error")]'
+    
+    def input_name(self, name):
+        name_input = self.driver.find_element_by_xpath(self._NAME_XPATH)
+        name_input.clear()
+        name_input.send_keys(name)
+        
+    def mark_all(self):
+        mark_all_radio = self.driver.find_element_by_xpath(self._MARK_ALL_XPATH)
+        mark_all_radio.click()
+    
+    def submit(self):
+        submit_btn = self.driver.find_element_by_xpath(self._SUBMIT_XPATH)
+        submit_btn.click()
+        check_invisible(self._SELF_XPATH, self.driver)
+    
+    def delete(self):
+        del_btn = self.driver.find_element_by_xpath(self._DELETE_XPATH)
+        del_btn.click()
+        check_visible(self._DEL_YES_XPATH, self.driver)
+        del_yes = self.driver.find_element_by_xpath(self._DEL_YES_XPATH)
+        del_yes.click()
+    
+    def check_has_source_error(self):
+        return check_visible(self._SOURCE_ERR_XPATH, self.driver)
+
+class AudienceElement(Component):
+    HDR_XPATH_TMPL = '//div[contains(@class, "audience-list-item__name") and text()="%s"]'
+    _FORM_XPATH_ADDITION = '/following-sibling::*[contains(@class, "audience-list-item__form")]/div[contains(@class, "audience-form")]'
+    
+    def __init__(self, driver, name):
+        Component.__init__(self, driver)
+        self.name = name
+        self.HDR_XPATH = AudienceElement.HDR_XPATH_TMPL % name
+    
+    def get_form(self):
+        return AudienceForm(self.driver, self.HDR_XPATH + AudienceElement._FORM_XPATH_ADDITION)
+    
+    def delete(self):
+        hdr = self.driver.find_element_by_xpath(self.HDR_XPATH)
+        hdr.click()
+        check_visible(self.HDR_XPATH + self._FORM_XPATH_ADDITION, self.driver)
+        aud_form = self.get_form()
+        aud_form.delete()
+        check_invisible(self.HDR_XPATH, self.driver)
+    
     
 class TargetTest(unittest.TestCase):
     USERNAME = u'name'
@@ -208,3 +290,26 @@ class TargetTest(unittest.TestCase):
         vk_app_adder = self.page.get_vk_app_adder()
         vk_app_adder.input_group(vk_app_name)
         self.assertTrue(vk_app_adder.check_has_helper())
+    
+    
+    def test_page_has_create_aud_btn(self):
+        self.assertTrue(self.page.has_create_aud_btn())
+    
+    def test_create_aud_filters_empty_source(self):
+        aud_name = "My auditory"
+        aud_form = self.page.get_create_aud_form()
+        aud_form.input_name(aud_name)
+        aud_form.submit()
+        self.assertTrue(aud_form.check_has_source_error())
+
+    def test_aud_creates(self):
+        aud_name = "My auditory"
+        aud_form = self.page.get_create_aud_form()
+        aud_form.input_name(aud_name)
+        aud_form.mark_all()
+        aud_form.submit()
+        aud_element = self.page.get_aud_element(aud_name)
+        self.assertIsNotNone(aud_element)
+        if aud_element is not None:
+            aud_element.delete()
+        
